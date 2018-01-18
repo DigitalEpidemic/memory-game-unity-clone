8b6e591737d4e62a16c33b155611bee76e64408a
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-18 04:22:50 -0500

    Refactored controller Game Objects and finished Load Selected Puzzle

--- Assets/Scripts/3 - Puzzle Game Controller/LoadPuzzleGame.cs
+++ Assets/Scripts/3 - Puzzle Game Controller/LoadPuzzleGame.cs
@@ -16,7 +16,74 @@ public class LoadPuzzleGame : MonoBehaviour {
 	[SerializeField]
 	private Animator puzzleGamePanelAnim1, puzzleGamePanelAnim2, puzzleGamePanelAnim3, puzzleGamePanelAnim4, puzzleGamePanelAnim5;
 
+	private int puzzleLevel;
+
+	private string selectedPuzzle;
+
 	public void LoadPuzzle (int level, string puzzle) {
+		this.puzzleLevel = level;
+		this.selectedPuzzle = puzzle;
+
+		switch (puzzleLevel) {
+
+		case 0:
+			StartCoroutine (LoadPuzzleGamePanel (puzzleGamePanel1, puzzleGamePanelAnim1));
+			break;
+		case 1:
+			StartCoroutine (LoadPuzzleGamePanel (puzzleGamePanel2, puzzleGamePanelAnim2));
+			break;
+		case 2:
+			StartCoroutine (LoadPuzzleGamePanel (puzzleGamePanel3, puzzleGamePanelAnim3));
+			break;
+		case 3:
+			StartCoroutine (LoadPuzzleGamePanel (puzzleGamePanel4, puzzleGamePanelAnim4));
+			break;
+		case 4:
+			StartCoroutine (LoadPuzzleGamePanel (puzzleGamePanel5, puzzleGamePanelAnim5));
+			break;
+
+		}
+
+	}
+
+	public void BackToPuzzleLevelSelectMenu () {
+		
+		switch (puzzleLevel) {
+
+		case 0:
+			StartCoroutine (LoadPuzzleLevelSelectMenu (puzzleGamePanel1, puzzleGamePanelAnim1));
+			break;
+		case 1:
+			StartCoroutine (LoadPuzzleLevelSelectMenu (puzzleGamePanel2, puzzleGamePanelAnim2));
+			break;
+		case 2:
+			StartCoroutine (LoadPuzzleLevelSelectMenu (puzzleGamePanel3, puzzleGamePanelAnim3));
+			break;
+		case 3:
+			StartCoroutine (LoadPuzzleLevelSelectMenu (puzzleGamePanel4, puzzleGamePanelAnim4));
+			break;
+		case 4:
+			StartCoroutine (LoadPuzzleLevelSelectMenu (puzzleGamePanel5, puzzleGamePanelAnim5));
+			break;
+
+		}
+		
+	}
+
+	IEnumerator LoadPuzzleLevelSelectMenu (GameObject puzzleGamePanel, Animator puzzleGamePanelAnim) {
+		puzzleLevelSelectPanel.SetActive (true);
+		puzzleLevelSelectAnim.Play ("SlideIn");
+		puzzleGamePanelAnim.Play ("SlideOut");
+		yield return new WaitForSeconds (1f);
+		puzzleGamePanel.SetActive (false);
+	}
+
+	IEnumerator LoadPuzzleGamePanel (GameObject puzzleGamePanel, Animator puzzleGamePanelAnim) {
+		puzzleGamePanel.SetActive (true);
+		puzzleGamePanelAnim.Play ("SlideIn");
+		puzzleLevelSelectAnim.Play ("SlideOut");
+		yield return new WaitForSeconds (1f);
+		puzzleLevelSelectPanel.SetActive (false);
 	}
 
 } // LoadPuzzleGame
