b6b63df817b340a38129cb1eafa59deaf37b1043
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-18 03:12:16 -0500

    Refactored folders and added Puzzle Level Controller

---
+++ Assets/Scripts/1 - Puzzle Menu Controller/SelectPuzzle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPuzzle : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject selectPuzzleMenuPanel, puzzleLevelSelectPanel;
+
+	[SerializeField]
+	private Animator selectPuzzleMenuAnim, puzzleLevelSelectAnim;
+
+	private string selectedPuzzle;
+
+	public void SelectedPuzzle () {
+		selectedPuzzle = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+
+		StartCoroutine (ShowPuzzleLevelSelectMenu ());
+	}
+
+	IEnumerator ShowPuzzleLevelSelectMenu () {
+		puzzleLevelSelectPanel.SetActive (true);
+		selectPuzzleMenuAnim.Play ("SlideOut");
+		puzzleLevelSelectAnim.Play ("SlideIn");
+		yield return new WaitForSeconds (1f);
+		selectPuzzleMenuPanel.SetActive (false);
+	}
+
+} // SelectPuzzle
---
+++ Assets/Scripts/2 - Puzzle Level Controller/SelectLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectLevel : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject selectPuzzleMenuPanel, puzzleLevelSelectPanel;
+
+	[SerializeField]
+	private Animator selectPuzzleMenuAnim, puzzleLevelSelectAnim;
+
+	private string selectedPuzzle;
+
+	public void BackToPuzzleSelectMenu () {
+		StartCoroutine (ShowPuzzleSelectMenu ());
+	}
+
+	IEnumerator ShowPuzzleSelectMenu () {
+		selectPuzzleMenuPanel.SetActive (true);
+		selectPuzzleMenuAnim.Play ("SlideIn");
+		puzzleLevelSelectAnim.Play ("SlideOut");
+		yield return new WaitForSeconds (1f);
+		puzzleLevelSelectPanel.SetActive (false);
+	}
+
+} // SelectLevel
