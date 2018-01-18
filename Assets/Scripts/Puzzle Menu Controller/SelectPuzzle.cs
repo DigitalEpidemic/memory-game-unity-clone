338f49188b95ecf852b657f7713aacf50b72f2ab
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-18 02:37:56 -0500

    Programmed the Settings Panel animations

---
+++ Assets/Scripts/Puzzle Menu Controller/SelectPuzzle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPuzzle : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject selectPuzzleMenuPanel;
+
+	[SerializeField]
+	private Animator selectPuzzleMenuAnim;
+
+	private string selectedPuzzle;
+
+	public void SelectedPuzzle () {
+		selectedPuzzle = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+
+		Debug.Log (selectedPuzzle);
+	}
+
+} // SelectPuzzle
---
+++ Assets/Scripts/Puzzle Menu Controller/SettingsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsController : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject settingsPanel;
+
+	[SerializeField]
+	private Animator settingsPanelAnim;
+
+	public void OpenSettingsPanel () {
+		settingsPanel.SetActive (true);
+		settingsPanelAnim.Play ("SlideIn");
+	}
+
+	public void CloseSettingsPanel () {
+		StartCoroutine (CloseSettings ());
+	}
+
+	IEnumerator CloseSettings () {
+		settingsPanelAnim.Play ("SlideOut");
+		yield return new WaitForSeconds (1f);
+		settingsPanel.SetActive (false);
+	}
+
+} // SettingsController
