f5273778c249f5c039a9a2a4bf22ccf3f663715c
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-19 01:37:10 -0500

    Added LevelLocker script

--- Assets/Scripts/1 - Puzzle Menu Controller/SelectPuzzle.cs
+++ Assets/Scripts/1 - Puzzle Menu Controller/SelectPuzzle.cs
@@ -11,6 +11,9 @@ public class SelectPuzzle : MonoBehaviour {
 	private SelectLevel selectLevel;
 
 	[SerializeField]
+	private LevelLocker levelLocker;
+
+	[SerializeField]
 	private GameObject selectPuzzleMenuPanel, puzzleLevelSelectPanel;
 
 	[SerializeField]
@@ -21,6 +24,7 @@ public class SelectPuzzle : MonoBehaviour {
 	public void SelectedPuzzle () {
 		selectedPuzzle = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
 		puzzleGameManager.SetSelectedPuzzle (selectedPuzzle);
+		levelLocker.CheckWhichLevelsAreUnlocked (selectedPuzzle);
 		selectLevel.SetSelectedPuzzle (selectedPuzzle);
 
 		StartCoroutine (ShowPuzzleLevelSelectMenu ());
---
+++ Assets/Scripts/2 - Puzzle Level Controller/LevelLocker.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLocker : MonoBehaviour {
+
+	[SerializeField]
+	private PuzzleGameSaver puzzleGameSaver;
+
+	[SerializeField]
+	private GameObject[] levelStarsHolder;
+
+	[SerializeField]
+	private GameObject[] levelsPadlocks;
+
+	private bool[] candyPuzzleLevels;
+	private bool[] transportPuzzleLevels;
+	private bool[] fruitPuzzleLevels;
+
+	void Awake () {
+		DeactivatePadlocksAndStarHolders ();
+	}
+
+	void Start () {
+		GetLevels ();
+	}
+
+	public void CheckWhichLevelsAreUnlocked (string selectedPuzzle) {
+		DeactivatePadlocksAndStarHolders ();
+		GetLevels ();
+
+		switch (selectedPuzzle) {
+
+		case "Candy Puzzle":
+			for (int i = 0; i < candyPuzzleLevels.Length; i++) {
+				if (candyPuzzleLevels [i]) {
+					levelStarsHolder [i].SetActive (true);
+				} else {
+					levelsPadlocks [i].SetActive (true);
+				}
+			}
+			break;
+
+		case "Transport Puzzle":
+			for (int i = 0; i < transportPuzzleLevels.Length; i++) {
+				if (transportPuzzleLevels [i]) {
+					levelStarsHolder [i].SetActive (true);
+				} else {
+					levelsPadlocks [i].SetActive (true);
+				}
+			}
+			break;
+
+		case "Fruit Puzzle":
+			for (int i = 0; i < fruitPuzzleLevels.Length; i++) {
+				if (fruitPuzzleLevels [i]) {
+					levelStarsHolder [i].SetActive (true);
+				} else {
+					levelsPadlocks [i].SetActive (true);
+				}
+			}
+			break;
+		}
+	}
+
+	void DeactivatePadlocksAndStarHolders () {
+		for (int i = 0; i < levelStarsHolder.Length; i++) {
+			levelStarsHolder [i].SetActive (false);
+			levelsPadlocks [i].SetActive (false);
+		}
+	}
+
+	void GetLevels () {
+		candyPuzzleLevels = puzzleGameSaver.candyPuzzleLevels;
+		transportPuzzleLevels = puzzleGameSaver.transportPuzzleLevels;
+		fruitPuzzleLevels = puzzleGameSaver.fruitPuzzleLevels;
+	}
+
+	public bool[] GetPuzzleLevels (string selectedPuzzle) {
+		switch (selectedPuzzle) {
+		case "Candy Puzzle":
+			return this.candyPuzzleLevels;
+			break;
+		case "Transport Puzzle":
+			return this.transportPuzzleLevels;
+			break;
+		case "Fruit Puzzle":
+			return this.fruitPuzzleLevels;
+			break;
+		default:
+			return null;
+			break;
+		}
+	}
+
+} // LevelLocker
